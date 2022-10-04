497ec22deec6c88b0ce348f75c952f5710e7623f
Luca Trimboli <106075804+LucaT003@users.noreply.github.com>
2022-10-04 16:57:47 +0200

    Add files via upload

---
+++ Es.Day2week2/src/Esercizio1/BrunoClass.java
@@ -0,0 +1,17 @@
+package Esercizio1;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class BrunoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> words = new ArrayList<>();
+		words.add( "primo" );
+		words.add( "secondo" );
+		words.add( "terzo" );
+
+	}
+
+}
---
+++ Es.Day2week2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author lucatrimboli
+ *
+ */
+module Es.Day2week2 {
+}
